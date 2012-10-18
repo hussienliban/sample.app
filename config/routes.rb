ace1e3bbf2a393b22635b23037f21e8e190cfea2
Hussien Liban <mntstyle@gmail.com>
2012-10-18 12:41:45 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/help"
 
-  get "staticpages/home"
+  get "static_pages/about"
 
-  get "staticpages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
